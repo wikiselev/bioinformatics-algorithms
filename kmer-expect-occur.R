6196f0a9b989ee08abe4145369e5c735533f5026
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2013-12-02 23:34:49 +0000

    Changed P to Pr

--- kmer-expect-occur.R
+++ kmer-expect-occur.R
@@ -5,17 +5,17 @@ N = 1000
 # number of letters in alphabet (A, C, G, T)
 A = 4
 
-# P(N, A, Pattern, t) denote the probability that a string Pattern appears t or
+# Pr(N, A, Pattern, t) denote the probability that a string Pattern appears t or
 # more times in a random string of length N formed from an alphabet of A letters.
-P <- function(N, A, k, t) {
+Pr <- function(N, A, k, t) {
 	return(choose(N - t*(k - 1), t)/A^(t*k))
 }
 
-# function that constructs a data frame of P(N, A, Pattern, t) for given N, n and t,
+# function that constructs a data frame of Pr(N, A, Pattern, t) for given N, n and t,
 # and varying k (here k = seq(1,12) - I am interested in k-mers with lengths 1..12)
-get_P_data_frame <- function(N, n, t) {
+get_Pr_data_frame <- function(N, n, t) {
 	d <- data.frame(N = N, A = A, k = seq(1,12), t = rep(t, each = 12), n = n)
-	d$P <- P(N, A, d$k, d$t)*n
+	d$Pr <- Pr(N, A, d$k, d$t)*n
 	# id is a unique line identifier - it is need for grouping in plotting with geom_line
 	d$id <- paste0(d$t, "_", d$n)
 	return(d)
@@ -25,19 +25,19 @@ get_P_data_frame <- function(N, n, t) {
 d <- NULL
 for (n in c(1, 500)) {
 	for (t in c(1, 2, 3)) {
-		d <- rbind(d, get_P_data_frame(N, n, t))
+		d <- rbind(d, get_Pr_data_frame(N, n, t))
 	}
 }
 
 # plot the total data frame
-p <- ggplot(d, aes(x = k, y = log10(P), group = id)) +
+p <- ggplot(d, aes(x = k, y = log10(Pr), group = id)) +
 	 geom_line(aes(color = as.factor(t), linetype = as.factor(n)), size = 1) +
 	 geom_hline(yintercept = 0) +
 	 coord_cartesian(ylim = c(-1, 9)) +
 	 scale_x_continuous(breaks=seq(1, 12, 1)) +
 	 scale_y_continuous(breaks=seq(-1, 9, 1)) +
-	 labs(x = "k", y = "log10(P*n)", 
-	 	title = paste0("Expected k-mer occurrences (P*n)\nin n strings of length ", N),
+	 labs(x = "k", y = "log10(Pr*n)", 
+	 	title = paste0("Expected k-mer occurrences (Pr*n)\nin n strings of length ", N),
 	 	color = "t", linetype = "n") +
 	 theme(text = element_text(size = 16))
 png(file = "figures/kmer-expect-occur.png", width = 600, height = 400)
